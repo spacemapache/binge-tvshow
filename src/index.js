49e8fa9155b56eec6fb8b84c7bcebe9f1717effd
spacemapache <142768034+spacemapache@users.noreply.github.com>
2023-12-06 15:59:26 +0800

    updated footer to include linkedin

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function joinFandom() {
   let name = prompt("What is your name?");
   let email = prompt("What is your email?");
 
-  alert(`Hi ${name}! Welcome to the Brooklyn Nine-Nine Fandom!`);
+  alert(`Hi ${name}! Welcome to the Brooklyn Nine-Nine Fandom!🚨💙`);
 }
 
 let joinFandomButton = document.querySelector("#join-fandom");
