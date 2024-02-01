234ed2ec93188e968ba53bbc8e503e595749e216
spacemapache <142768034+spacemapache@users.noreply.github.com>
2024-02-01 14:33:43 -0800

    removed js and css file as submission only allows a html file

--- src/index.js
+++
@@ -1,9 +0,0 @@
-function joinFandom() {
-  let name = prompt("What is your name?");
-  let email = prompt("What is your email?");
-
-  alert(`Hi ${name}! Welcome to the Brooklyn Nine-Nine Fandom!🚨💙`);
-}
-
-let joinFandomButton = document.querySelector("#join-fandom");
-joinFandomButton.addEventListener("click", joinFandom);
